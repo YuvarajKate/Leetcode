3f2eb674bb9c4263769d9c40cb26eba9550d7a65
Yuvaraj Kate <84085324+YuvarajKate@users.noreply.github.com>
2024-10-08 12:43:52 +0530

    Time: 407 ms (91.12%), Space: 32 MB (25.36%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,11 +1,9 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
-        nums.sort()
-        for i in range(1, len(nums)):
-            if nums[i] == nums[i - 1]:
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
                 return True
+            hashset.add(n)
         return False
