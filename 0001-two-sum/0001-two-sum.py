89b9b79babed222adf26d1938e80f939cb5bbc17
YuvarajKate <84085324+YuvarajKate@users.noreply.github.com>
2025-09-27 20:42:12 +0500

    Time: 1755 ms (24.48%), Space: 18.4 MB (86.95%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        pmap = {}
-        for i, n in enumerate(nums):
-            x = target - n
-            if x in pmap:
-                return [pmap[x] , i]
-            
-            pmap[n] = i
-            
+        for i in range(len(nums)):
+            for j in range((i+1),len(nums)):
+                if (nums[i]+nums[j])==target:
+                    return [i,j]
