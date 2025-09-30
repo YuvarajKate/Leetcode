b60657bc58a0186a1a18ad176baec1e3a84aadb6
YuvarajKate <84085324+YuvarajKate@users.noreply.github.com>
2025-09-30 22:06:28 +0500

    Time: 3 ms (29.75%), Space: 18.1 MB (13.6%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,14 +1,29 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = []
-        closetoopen = {"]":"[", ")":"(", "}":"{"}
+        l = list(s)
 
-        for c in s:
-            if c in closetoopen:
-                if stack and stack[-1] == closetoopen[c]:
-                    stack.pop()
-                else:
-                    return False
+        parenthesis = {')': '(', '}': '{', ']': '['}
+        check = []
+        x = True  
+        for i in l:
+            # Pushing on Stack
+            if i not in parenthesis:
+                check.append(i)
             else:
-                stack.append(c)
-        return True if not stack else False
+                # If check is empty 
+                if check == []:
+                    x = False   
+                    break
+                
+                top = check.pop()  
+                #matching with the poped element 
+                if parenthesis[i] != top:
+                    x = False   
+                    break
+
+        # At end stack must be empty
+        if check:
+            x = False
+
+        return x
+
