f2c9deca93c3338f90a050efdf603b1c87394f6f
YuvarajKate <84085324+YuvarajKate@users.noreply.github.com>
2025-10-17 22:09:45 +0530

    Time: 0 ms (100%), Space: 17.7 MB (48.69%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -3,6 +3,6 @@ class Solution:
         if n <= 2:
             return n
         first, second = 1, 2
-        for _ in range(3, n + 1):
+        for i in range(3, n + 1):
             first, second = second, first + second
         return second
