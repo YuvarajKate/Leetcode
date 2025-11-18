a32ee3c724ff3dbfbee22942faa421b1955883c7
YuvarajKate <84085324+YuvarajKate@users.noreply.github.com>
2025-11-18 23:22:23 +0530

    Time: 0 ms (100%), Space: 18 MB (23.71%) - LeetHub

--- 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.py
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.py
@@ -1,9 +1,12 @@
 class Solution:
     def isOneBitCharacter(self, bits: List[int]) -> bool:
-        op = 0
-        bits = bits[::-1]
-        while bits:
-            op = bits.pop()
-            if op:
-                bits.pop()
-        return not op
+        count = 0
+        i = len(bits) - 2       # start just before the guaranteed last 0
+
+        # count consecutive 1s backwards
+        while i >= 0 and bits[i] == 1:
+            count += 1
+            i -= 1
+
+        # if count of 1s is even → last char is 1-bit
+        return (count % 2 == 0)
